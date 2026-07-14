ca0b339e55e717021c8f6d04bf1e5c48903e8176
Robert Derbyshire <dev.derbyshire@gmail.com>
2026-07-14 13:32:20 +0100

    Added handler for logging out the user

---
+++ backend/src/main/kotlin/uk/derbyshire/api/auth/postLogout.kt
@@ -0,0 +1,19 @@
+package uk.derbyshire.api.auth
+
+import org.http4k.core.HttpHandler
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.cookie.cookie
+import uk.derbyshire.ServerConfig
+import uk.derbyshire.api.helpers.SESSION_COOKIE_NAME
+import uk.derbyshire.api.helpers.expiredSessionCookie
+import uk.derbyshire.services.AuthService
+
+fun postLogout(authService: AuthService, serverConfig: ServerConfig): HttpHandler = { request: Request ->
+    request.cookie(SESSION_COOKIE_NAME)?.let {
+        authService.logout(it.value)
+    }
+
+    Response(Status.NO_CONTENT).cookie(expiredSessionCookie(serverConfig.secureSessionCookies))
+}
