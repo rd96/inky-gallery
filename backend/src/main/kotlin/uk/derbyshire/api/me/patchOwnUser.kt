ff792539eaa35b1f2bdf6f7d57ee3f44cbd20f57
Robert Derbyshire <dev.derbyshire@gmail.com>
2026-07-24 21:09:56 +0100

    Added endpoint to update your display name

---
+++ backend/src/main/kotlin/uk/derbyshire/api/me/patchOwnUser.kt
@@ -0,0 +1,38 @@
+package uk.derbyshire.api.me
+
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Success
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import uk.derbyshire.api.filters.CurrentUser
+import uk.derbyshire.api.filters.ErrorResponseDTO.Companion.toErrorResponseDTO
+import uk.derbyshire.api.helpers.Json
+import uk.derbyshire.services.UserService
+
+fun patchOwnUser(userService: UserService) = { request: Request ->
+    val currentUser = CurrentUser(request)
+
+    val patchUserRequest = PatchOwnUserRequestDTO.lens(request)
+
+    val result = userService.updateUser(
+        userId = currentUser.userId,
+        username = null,
+        displayName = patchUserRequest.displayName,
+        enabled = null,
+        role = null,
+    )
+
+    when (result) {
+        is Success -> Response(Status.OK)
+        is Failure -> result.reason.description.toErrorResponseDTO()
+    }
+}
+
+data class PatchOwnUserRequestDTO(
+    val displayName: String,
+) {
+    companion object {
+        val lens = Json.autoBody<PatchOwnUserRequestDTO>().toLens()
+    }
+}
